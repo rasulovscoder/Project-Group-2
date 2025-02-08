f5652c83c618671a19a3a32391b6e31d2dfabfe3
Nurislom1006 <nurislomalisherovich@gmail.com>
2025-02-08 19:34:34 +0500

    Nurislom
    Details.jsx yuklandi

---
+++ src/Details/Details.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import rasm1 from "./img/rasm2.png"
+
+const Details = () => {
+  return (
+    <div>
+        <div className='w-[1440px] h-[810px] '>
+            <div>
+                <img src={rasm1} className='w-[774px] h-[549px] ' />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Details
