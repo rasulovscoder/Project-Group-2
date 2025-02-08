7da29840ee932fec04f42043c7a41bc7de96f9a5
Nurislom1006 <nurislomalisherovich@gmail.com>
2025-02-08 19:40:43 +0500

    Nurislom
    Details papka ochildi

--- src/Details/Details.jsx
+++ src/Details/Details.jsx
@@ -7,6 +7,11 @@ const Details = () => {
         <div className='w-[1440px] h-[810px] '>
             <div>
                 <img src={rasm1} className='w-[774px] h-[549px] ' />
+                <div>
+                    <h1 className='text-[56px] font-[700]'>Vanguard CX2
+                        <br />
+                    Convertible</h1>
+                </div>
             </div>
         </div>
     </div>
