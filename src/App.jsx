a48b9434c16053416c71c7a35820a38799fe2c66
Fayzulloh190 <farhodbekmaksudov64@gmail.com>
2025-02-08 19:17:09 +0500

    Fayzulloh

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import Service from './Service/Service'
 
 const App = () => {
   return (
     <div>
-    
-
+<Service></Service>
 
     </div>
   )
--- src/Service/Service.jsx
+++ src/Service/Service.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import rasm1 from './images/1.png'
 
 const Service = () => {
   return (
     <div>
-<div className='w-[1440px] h-[113px]'>
-      <h1>Home/About us</h1>
-      <h1>Who We are</h1>
+<div className='w-[1440px] h-[113px] text-center bg-white'>
+      <h1 className='text-[20px] text-black '>Home/About us</h1>
+      <h1 className='text-[40px] text-black font-bold'>Who We are</h1>
     </div>
+      <img src={rasm1} className='w-[1440px] h-[490px] bg-white' />
+
+
 
 
     </div>
