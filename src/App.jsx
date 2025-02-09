11bde0073df18ac43ea19eb362cbbc71e0793a0a
Nurislom1006 <nurislomalisherovich@gmail.com>
2025-02-09 10:45:36 +0500

    Nurislom
    Router domga hamma narsa ulandi

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Service from './Service/Service'
+import Details from './Details/Details'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import Home from './Home/Home'
+import Faq from './FAQ\'s/Faq'
 
 
 const App = () => {
   return (
     <div >
-<Service></Service>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/service' element={<Service />} />
+          <Route path='/details' element={<Details />} />
+          <Route path='/faq' element={<Faq />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
