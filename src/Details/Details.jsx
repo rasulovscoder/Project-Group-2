e359559d40109addb39448210d4950aa22336980
Nurislom1006 <nurislomalisherovich@gmail.com>
2025-02-10 20:48:19 +0500

    Nurislom

--- src/Details/Details.jsx
+++ src/Details/Details.jsx
@@ -9,6 +9,15 @@ import rasm7 from "./img/Vector1.png"
 import rasm8 from "./img/Vector2.png"
 import rasm9 from "./img/Vector3.png"
 import rasm10 from "./img/Shape.png"
+import rasm11 from "./img/Moshina.png"
+import rasm12 from "./img/Musor.png"
+import rasm13 from "./img/Mashina.png"
+import rasm14 from "./img/Bugatti.png"
+import rasm15 from "./img/ranger.png"
+import rasm16 from "./img/cls.jpg"
+
+
+
 
 
 
@@ -23,17 +32,17 @@ const Details = () => {
 
     return (
         <div>
-            <div className='w-[1440px] h-[810px] '>
+            <div className='w-[1440px] h-[810px] m-auto'>
                 <div className=' w-full h-[317px] bg-center bg-no-repeat bg-cover bg-[url(./img/rasm1.jpg)] bg-red-400'>
 
                 </div>
                 <div className='flex flex-row'>
                     <div>
-                        <img src={rasm1} className='w-[774px] h-[549px] ' />
-                        <div className='w-[774px] h-[240px] flex justify-between'>
-                            <img src={rasm3} className="w-[240px] h-[240px]" />
-                            <img src={rasm4} className="w-[240px] h-[240px]" />
-                            <img src={rasm5} className="w-[240px] h-[240px]" />
+                        <img src={rasm1} className='w-[900px] h-[549px] ' />
+                        <div className='w-[900px] h-[240px] flex justify-between'>
+                            <img src={rasm3} className="w-[300px] h-[240px]" />
+                            <img src={rasm4} className="w-[300px] h-[240px]" />
+                            <img src={rasm5} className="w-[300px] h-[240px]" />
                         </div>
                     </div>
                     <div>
@@ -125,7 +134,7 @@ const Details = () => {
                     </div>
 
                 </div>
-                <div className='w-[1440px] h-[393px] px-[50px] mt-[30px]'>
+                <div className='w-[1440px] h-[393px] px-[50px] mt-[100px] flex flex-col gap-[56px] '>
                     <div className='w-[1340px] h-[58px] flex flex-row items-center justify-between'>
                         <h1 className='text-[48px] font-[500] '>You may also like</h1>
                         <div className='w-[124px] h-[50px] flex justify-between items-center'>
@@ -137,8 +146,70 @@ const Details = () => {
                             </div>
                         </div>
                     </div>
+                    <div className='w-[1340px] h-[279px] flex flex-row gap-[35px]'>
+                        <div className='w-[240px] h-[279px] pt-[12px] pr-[12px] pl-[12px] pb-[20px] bg-[#F8F8F8] flex flex-col gap-[10px] rounded-[10px] duration-200 hover:bg-red-500'>
+                            <img src={rasm11} className="w-[216px] h-[160px]" />
+                            <div className='flex  flex-col items-center'>
+                                <h1 className='text-[16px] font-[700]'>Horizon Mirage</h1>
+                                <h1 className='text-[16px] font-[700]'> Convertible</h1>
+                                <p className='text-[16px] font-[600] mt-[10px]'>$59/day</p>
+                            </div>
+                        </div>
+                        <div className='w-[240px] h-[279px] pt-[12px] pr-[12px] pl-[12px] pb-[20px] bg-[#F8F8F8] flex flex-col gap-[10px] rounded-[10px] duration-200 hover:bg-red-500'>
+                            <img src={rasm12} className="w-[216px] h-[160px]" />
+                            <div className='flex  flex-col items-center'>
+                                <h1 className='text-[16px] font-[700]'>Vanguard Phoenix</h1>
+                                <h1 className='text-[16px] font-[700]'> C-Class</h1>
+                                <p className='text-[16px] font-[600] mt-[10px]'>$49/day</p>
+                            </div>
+                        </div>
+                        <div className='w-[240px] h-[279px] pt-[12px] pr-[12px] pl-[12px] pb-[20px] bg-[#F8F8F8] flex flex-col gap-[10px] rounded-[10px] duration-200 hover:bg-red-500'>
+                            <img src={rasm13} className="w-[216px] h-[160px]" />
+                            <div className='flex  flex-col items-center'>
+                                <h1 className='text-[16px] font-[700]'>Apex Autos Typhoon</h1>
+                                <h1 className='text-[16px] font-[700]'> Model 3</h1>
+                                <p className='text-[16px] font-[600] mt-[10px]'>$59/day</p>
+                            </div>
+                        </div>
+                        <div className='w-[240px] h-[279px] pt-[12px] pr-[12px] pl-[12px] pb-[20px] bg-[#F8F8F8] flex flex-col gap-[10px] rounded-[10px] duration-200 hover:bg-red-500'>
+                            <img src={rasm14} className="w-[216px] h-[160px]" />
+                            <div className='flex  flex-col items-center'>
+                                <h1 className='text-[16px] font-[700]'>Radiant Solstice </h1>
+                                <h1 className='text-[16px] font-[700]'> Convertible</h1>
+                                <p className='text-[16px] font-[600] mt-[10px]'>$79/day</p>
+                            </div>
+                        </div>
+                        <div className='w-[240px] h-[279px] pt-[12px] pr-[12px] pl-[12px] pb-[20px] bg-[#F8F8F8] flex flex-col gap-[10px] rounded-[10px] duration-200 hover:bg-red-500'>
+                            <img src={rasm15} className="w-[216px] h-[160px]" />
+                            <div className='flex  flex-col items-center'>
+                                <h1 className='text-[16px] font-[700]'>Aurora X5 Nebula</h1>
+                                <h1 className='text-[16px] font-[700]'> Minivan</h1>
+                                <p className='text-[16px] font-[600] mt-[10px]'>$59/day</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
+                <div className='w-[1440px] h-[400px] px-[50px] mt-[100px] '>
+                    <div className='w-[1340px] h-[400px] rounded-[30px] bg-[url(./img/cls.jpg)]'>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h1 className='text-[56px] font-[700] '>Book Your Adventure</h1>
+                            <h1 className='text-[56px] font-[700] '>Today and Feel the Power</h1>
+                            <h1 className='text-[56px] font-[700] '>of the Open Road.</h1>
+                            <br /><br />
+                            <button className='w-[212px] h-[40px] rounded-[32px] border-red-500 duration-200 text-black border-[2px] text-[16px] font-[500] hover:bg-red-500 hover:text-white'>Let's Drive with Us</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='w-[1440px] h-[320px] flex flex-row p-[50px] '>
+                    <div className='w-[552px] h-[220px] flex flex-col'>
+                        <h1 className='text-[60px] font-[700]'>Don't Miss a Thing</h1>
+                        <p className='text-[18px] font-[500] text-gray-400'>Subscribe to our newsletter for exclusive deals and updates.</p>
+                        <div className='w-[547px] h-[56px] flex flex-row justify-between mt-[55px] '>
+                            <input className='px-[15px] w-[500px] h-[56px] rounded-s-3xl border-r-white border-[2px] border-gray-500' type="text" placeholder='Enter email address for newsletter ...' />
+                            <div className='ml-[-4px] w-[50px] h-[56px] flex justify-center border-l-white items-center rounded-r-3xl border-[2px] border-gray-500'><i className="text-[25px] fa-solid fa-arrow-right-long"></i></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
